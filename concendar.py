740d2c99b7259124a11582741a1324e0a40b3b2d
juaangarciaa05 <jgarlan1208@m.educaand.es>
2023-03-12 16:50:30 +0100

    Refactor

--- concendar.py
+++ concendar.py
@@ -1,6 +1,11 @@
     #Concendar
-conciertos = {}
-
+import pickle
+conciertos = []
+try:
+    with open("conciertos.txt", "rb") as archivo:
+        conciertos = pickle.load(archivo)
+except FileNotFoundError:
+    pass
 print("Bienvenido a Concendar!    * Menú principal. Seleccione una opción")
 print("1) Mis conciertos")
 print("2) Mis lugares")
@@ -11,87 +16,45 @@ print("5) Salir")
 opcion = input("Seleccione una opción: ")
 
 def listar_conciertos():
-    import json
-    try:
-        with open("conciertos.json", "r") as f:
-            conciertos = json.load(f)
-    except FileNotFoundError:
-        with open("conciertos.json", "w") as f:
-            pass
     for c in conciertos:
         print(c)
 
 def listar_conciertos_por_lugar():
-    import json
-    try:
-        with open("conciertos.json", "r") as f:
-            conciertos = json.load(f)
-    except FileNotFoundError:
-        with open("conciertos.json", "w") as f:
-            pass
-    for c in conciertos:
-        print(c)
-
     lugar = input("Lugar del que quieres ver los conciertos: ")
     conciertos_filtrados = filter(lambda c: c["lugar"] == lugar , conciertos)
     for c in conciertos_filtrados:
         print(c)
 
 def añadir_concierto():
-    import json
-    try:
-        with open("conciertos.json", "r") as f:
-            conciertos = json.load(f)
-    except FileNotFoundError:
-        with open("conciertos.json", "w") as f:
-            pass
-    for c in conciertos:
-        print(c)
-    
-    id = input("Introduce la ID del concierto: ")
+    id = input ("Introduce el nombre del concierto: ")
     fecha = input("Introduce la fecha del concierto: ")
-    artista = input("Introduce el artista del concierto: ")
+    artista = input("Introduce el artista del concierto: ")  
     lugar = input("Introduce el lugar del concierto: ")
     precio = input("Introduce el precio del concierto: ")
     entrada = input("¿Tienes entrada para el concierto? (si o no): ")
     anotacion = input("Introduce alguna anotación del concierto si la necesita: ")
 
-    concierto = {
-        "id": id,
-        "fecha": fecha,
-        "artista": artista,
-        "lugar": lugar,
-        "precio": precio, 
-        "entrada": entrada,
-        "anotacion": anotacion
-    }
+    nuevo_concierto = {"id": id, "fecha": fecha, "artista": artista, "lugar": lugar, "precio": precio, "entrada": entrada, "anotacion": anotacion}
+    conciertos.append(nuevo_concierto)
 
-    conciertos[id] = concierto
-
-    with open("conciertos.json", "w") as f:
-        json.dump(conciertos, f)
+    with open("conciertos.txt", "wb") as archivo:
+        pickle.dump(conciertos, archivo)
+    
+    print("Se ha añadido el concierto correctamente")
 
 def eliminar_concierto():
-    import json
-    try:
-        with open("conciertos.json", "r") as f:
-            conciertos = json.load(f)
-    except FileNotFoundError:
-        with open("conciertos.json", "w") as f:
-            pass
-    for c in conciertos:
-        print(c)
-    
     id = input("Introduce la ID del concierto que quieres eliminar: ")
     for c in conciertos:
         if c["id"] == id:
             conciertos.remove(c)
-            print("Has eliminado el concierto")
             break
         else:
             print("No hay ningún concierto con ese id")
-    with open("conciertos.json", "w") as f:
-        json.dump(conciertos, f)    
+            break
+    with open("conciertos.txt", "wb") as archivo:
+        pickle.dump(conciertos, archivo)
+
+    print("Se ha eliminado el concierto correctamente")
 
 if opcion == "1":
     listar_conciertos()
