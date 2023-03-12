75c282de7e6cdeaf875902e1b6fd0069ce97f337
juaangarciaa05 <jgarlan1208@m.educaand.es>
2023-03-12 01:49:36 +0100

    Refactor

--- concendar.py
+++ concendar.py
@@ -1,74 +1,114 @@
     #Concendar
-#Menú
-def mostrar_menu(nombre, opciones):  
-    print(f'* {nombre}. Selecciona una opción:')
-    for clave in sorted(opciones):
-        print(f' {clave}) {opciones[clave][0]}')
+conciertos = {}
 
+print("Bienvenido a Concendar!    * Menú principal. Seleccione una opción")
+print("1) Mis conciertos")
+print("2) Mis lugares")
+print("3) Añadir un nuevo concierto")
+print("4) Eliminar un concierto")
+print("5) Salir")
 
-def leer_opcion(opciones):
-    while (a := input('Opción: ')) not in opciones:
-        print('Opción incorrecta, vuelva a intentarlo.')
-    return a
+opcion = input("Seleccione una opción: ")
 
+def listar_conciertos():
+    import json
+    try:
+        with open("conciertos.json", "r") as f:
+            conciertos = json.load(f)
+    except FileNotFoundError:
+        with open("conciertos.json", "w") as f:
+            pass
+    for c in conciertos:
+        print(c)
 
-def ejecutar_opcion(opcion, opciones):
-    opciones[opcion][1]()
+def listar_conciertos_por_lugar():
+    import json
+    try:
+        with open("conciertos.json", "r") as f:
+            conciertos = json.load(f)
+    except FileNotFoundError:
+        with open("conciertos.json", "w") as f:
+            pass
+    for c in conciertos:
+        print(c)
 
+    lugar = input("Lugar del que quieres ver los conciertos: ")
+    conciertos_filtrados = filter(lambda c: c["lugar"] == lugar , conciertos)
+    for c in conciertos_filtrados:
+        print(c)
 
-def generar_menu(nombre, opciones, opcion_salida):
-    opcion = None
-    while opcion != opcion_salida:
-        mostrar_menu(nombre, opciones)
-        opcion = leer_opcion(opciones)
-        ejecutar_opcion(opcion, opciones)
-        print()
+def añadir_concierto():
+    import json
+    try:
+        with open("conciertos.json", "r") as f:
+            conciertos = json.load(f)
+    except FileNotFoundError:
+        with open("conciertos.json", "w") as f:
+            pass
+    for c in conciertos:
+        print(c)
+    
+    id = input("Introduce la ID del concierto: ")
+    fecha = input("Introduce la fecha del concierto: ")
+    artista = input("Introduce el artista del concierto: ")
+    lugar = input("Introduce el lugar del concierto: ")
+    precio = input("Introduce el precio del concierto: ")
+    entrada = input("¿Tienes entrada para el concierto? (si o no): ")
+    anotacion = input("Introduce alguna anotación del concierto si la necesita: ")
 
-
-def menu_principal():
-    opciones = {
-        '1': ('Opción 1', funcion1),
-        '2': ('Opción 2 >', submenu),
-        '3': ('Opción 3', funcion3),
-        '4': ('Salir', salir)
+    concierto = {
+        "id": id,
+        "fecha": fecha,
+        "artista": artista,
+        "lugar": lugar,
+        "precio": precio, 
+        "entrada": entrada,
+        "anotacion": anotacion
     }
 
-    generar_menu('Menú principal', opciones, '4') 
+    conciertos[id] = concierto
+
+    with open("conciertos.json", "w") as f:
+        json.dump(conciertos, f)
+
+def eliminar_concierto():
+    import json
+    try:
+        with open("conciertos.json", "r") as f:
+            conciertos = json.load(f)
+    except FileNotFoundError:
+        with open("conciertos.json", "w") as f:
+            pass
+    for c in conciertos:
+        print(c)
+    
+    id = input("Introduce la ID del concierto que quieres eliminar: ")
+    for c in conciertos:
+        if c["id"] == id:
+            conciertos.remove(c)
+            print("Has eliminado el concierto")
+            break
+        else:
+            print("No hay ningún concierto con ese id")
+    with open("conciertos.json", "w") as f:
+        json.dump(conciertos, f)    
+
+if opcion == "1":
+    listar_conciertos()
+           
+elif opcion == "2":
+    listar_conciertos_por_lugar()
+
+elif opcion == "3":
+    añadir_concierto()
+    
+elif opcion == "4":
+    eliminar_concierto()
+    
+elif opcion == "5": 
+    print("Saliendo... Gracias por usar Concendar!")
 
 
-def submenu():
-    opciones = {
-        'a': ('Opción a', funcionA),
-        'b': ('Opción b', funcionB),
-        'c': ('Volver al menú principal', salir)
-    }
+else:
+    print("Opción inválida, seleccione otra por favor")
 
-    generar_menu('Submenú', opciones, 'c') 
-
-
-def funcion1():
-    print('Has elegido la opción 1')
-
-
-def funcion2():
-    print('Has elegido la opción 2')
-
-
-def funcion3():
-    print('Has elegido la opción 3')
-
-
-def funcionA():
-    print('Has elegido la opción A')
-
-
-def funcionB():
-    print('Has elegido la opción B')
-
-
-def salir():
-    print('Saliendo')
-
-
-if __name__ == '__main__':
-    menu_principal()
